358c5f3617fdb3be00fa5b26e893ef2c38f57c77
unknown <gi_douag@esi.dz>
2021-04-11 11:30:46 +0100

    new

---
+++ app/src/main/java/com/example/s2e1/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.s2e1
+
+import android.os.Build
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import java.time.LocalDate
+import java.time.Period
+
+
+class MainActivity : AppCompatActivity() {
+    //Calculer AGE
+    fun calculeAge(annee: Int, mois: Int, jours: Int): Int {
+        return Period.between(
+                LocalDate.of(annee, mois, jours),
+                LocalDate.now()
+        ).years
+    }
+    
+    //Récupérer Nom du périphérique
+    fun getDeviceName(): String? {
+        val manufacturer = Build.MANUFACTURER
+        val model = Build.MODEL
+        return if (model.startsWith(manufacturer)) {
+            model
+        } else {
+            manufacturer.toString() + " " + model
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            val age = calculeAge(1998,11,17)
+            Snackbar.make(view, age.toString(), Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+
+}
